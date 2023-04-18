5a31f5c698171130c670a410ae2d9e629d85e02c
Krystian Kubiczek <kkubiczek00@gmail.com>
2023-04-18 15:41:14 +0200

    add sticky menu

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 
 const hamburger = document.querySelector('.site-header__icon');
 
+const hamburgerBlack = document.querySelector('.nav-stick-icon');
+
 const close = document.querySelector('.menu .material-icons');
 
 const nav = document.querySelector('.menu');
@@ -21,3 +23,30 @@ close.addEventListener('click', ()=> {
     div.classList.remove('site-overlay');
 
 })
+
+hamburgerBlack.addEventListener('click', ()=>{
+
+    nav.classList.add('menu-show');
+    div.classList.add('site-overlay');
+
+} )
+
+close.addEventListener('click', ()=> {
+
+    nav.classList.remove('menu-show');
+    div.classList.remove('site-overlay');
+
+})
+
+const stickyNav = document.querySelector('.nav-stick');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        stickyNav.classList.add('nav-stick-display');
+    }else {
+        stickyNav.classList.remove('nav-stick-display');
+    }
+};
